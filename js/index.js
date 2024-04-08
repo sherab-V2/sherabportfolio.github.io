d0533b3700c745addf8ae3d876a917945a824297
20230925@dfei.ie <you@example.com>
2024-04-08 11:18:31 +0100

    lotsa stuff

--- js/index.js
+++ js/index.js
@@ -1,4 +1,79 @@
-$("#leftside-navigation .sub-menu > a").click(function(e) {
-    $("#leftside-navigation ul ul").slideUp(), $(this).next().is(":visible") || $(this).next().slideDown(),
-    e.stopPropagation()
-  })
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
+var renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+renderer.outputColorSpace = THREE.SRGBColorSpace;
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setClearColor(0x000000);
+renderer.setClearColor( 0xffffff, 0);
+renderer.setPixelRatio(window.devicePixelRatio);
+
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+document.body.appendChild(renderer.domElement);
+
+const scene = new THREE.Scene();
+
+// There's no reason to set the aspect here because we're going
+// to set it every frame anyway so we'll set it to 2 since 2
+// is the the aspect for the canvas default size (300w/150h = 2)
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.enablePan = false;
+controls.minDistance = 10;
+controls.maxDistance = 10;
+controls.minPolarAngle = 0.9;
+controls.maxPolarAngle = 1.5;
+controls.autoRotate = true;
+controls.update();
+
+
+
+
+
+var light = new THREE.PointLight(0xffffff, 10);
+light.position.set(-10,-90,-100);
+scene.add(light);
+
+
+var light2 = new THREE.PointLight(0xffffff, 10);
+light2.position.set(-10, -90,100);
+scene.add(light2);
+
+
+
+const loader = new GLTFLoader().setPath('/resources/3D/canti/');
+loader.load('scene.gltf', (gltf) => {
+  const mesh = gltf.scene;
+
+  mesh.traverse((child) => {
+    if (child.isMesh) {
+      child.castShadow = true;
+      child.receiveShadow = true;
+    }
+  });
+ 
+  
+  mesh.position.set(3, 0, -1.5);
+  scene.add(mesh);
+
+}, ( xhr ) => {
+  document.getElementById('container3d').innerHTML = `LOADING ${Math.max(xhr.loaded / xhr.total, 1) * 100}/100`;
+},);
+window.addEventListener('resize', () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
+
+
+function animate() {
+  requestAnimationFrame(animate);
+  controls.update();
+  renderer.render(scene, camera);
+}
+animate();
